a7b7c27e359705530b2505210a32785a1d465b86
vinay1723 <vinaykumar.17july@gmail.com>
2026-04-21 14:58:02 +0530

    Atstool Back-End setup

---
+++ Back-End/ATStool/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ATStool.Models;
+
+namespace ATStool.Data
+{
+    public class AppDbContext:IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options
+            ):base(options)
+        {
+
+        }
+    }
+}
---
+++ Back-End/ATStool/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ATStool.Models
+{
+    public class User:IdentityUser
+    {
+        public string FullName { get; set; }
+        public string UserType { get; set; }
+    }
+}
